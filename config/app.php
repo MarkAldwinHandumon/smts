20aefb08136424c6d44b2efde3563657de863703
viper357621 <87564641+viper357621@users.noreply.github.com>
2024-09-24 18:11:48 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -53,7 +53,7 @@ return [
     */
 
     // 'url' => env('APP_URL', 'http://localhost'),
-    'url' => env('APP_URL', 'http://sms.test'),
+    'url' => env('APP_URL', 'http://smts.test'),
 
     /*
     |--------------------------------------------------------------------------
